b297a4cf8aff72a0d620cf0e93b9127a35ca921e
skalitko <woof.1987@gmail.com>
2018-10-26 01:31:51 +0300

    +Player and tests

---
+++ Test/sk/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package sk.model;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @org.junit.Test
+    public void getName() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getFigure() throws Exception {
+    }
+
+}
