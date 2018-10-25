a1abd33e92f228f6c9ee3d0cf5c94f42200630cf
skalitko <woof.1987@gmail.com>
2018-10-26 01:45:22 +0300

    first tests

--- Test/sk/model/PlayerTest.java
+++ Test/sk/model/PlayerTest.java
@@ -1,14 +1,29 @@
 package sk.model;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PlayerTest {
     @org.junit.Test
     public void getName() throws Exception {
+
+        final String name = "Ivan";
+
+        final Player player = new Player(name, null);
+
+        assertEquals(name, player.getName());
+
     }
 
     @org.junit.Test
     public void getFigure() throws Exception {
+
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        assertEquals(expectedValue, inputValue);
     }
 
 }
